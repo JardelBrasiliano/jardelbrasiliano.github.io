67e4e359ed269935d2f234d7897d10ffcdca3d07
Jardel Brasiliano <54712359+JardelBrasiliano@users.noreply.github.com>
2023-10-28 19:33:35 -0300

    Update projetos.js

--- web/js/services/projetos.js
+++ web/js/services/projetos.js
@@ -1,27 +1,9 @@
 const listaProjetos = [
   {
-    "name": "Meu deck pokemon",
-    "descricao": "Consumindo a PokeAPI, crie seu deck e compartilhe com seus colegas.",
-    "url": "https://github.com/JardelBrasiliano/meu-deck-pokemon",
-    "topicos": ["React", "Firebase", "Redux", "Redux-saga", "Axios"]
-  },
-  {
-    "name": "Daminha",
-    "descricao": "Jogo de dama para ser jogado offline.",
-    "url": "https://github.com/JardelBrasiliano/daminha-game",
-    "topicos": ["React", "Context Api"]
-  },
-  {
-    "name": "Happy nlw firebase",
-    "descricao": "Implementação do firebase no projeto #NextLevelWeek3 da @Rocketseat.",
-    "url": "https://github.com/JardelBrasiliano/happy-nlw-firebase",
-    "topicos": ["React", "Firebase", "LeafletJs"]
-  },
-  {
-    "name": "S-eta",
-    "descricao": "SETA (Simulador de Estação de tratamento de Água), game quiz de química.",
-    "url": "https://github.com/JardelBrasiliano/s-eta",
-    "topicos": ["React", "HTML", "CSS"]
+    "name": "Simulado Detran Brasil",
+    "descricao": "Simulado Detran Brasil é um aplicativo híbrido feito em React Native onde os usuários podem se prepara para a prova teórica do detran.",
+    "url": "https://play.google.com/store/apps/details?id=com.simuladodetranbrasilfree",
+    "topicos": ["TypeScript", "React Native", "Firebase", "AdMob"]
   },
   {
     "name": "Cardaphia",
@@ -30,11 +12,23 @@ const listaProjetos = [
     "topicos": ["React", "Axios"]
   },
   {
-    "name": "Dininho adventures",
-    "descricao": "Estudo de Html, Css e Javascript",
-    "url": "https://github.com/JardelBrasiliano/dininho-adventures",
-    "topicos": ["HTML", "CSS", "JavaScript"]
+    "name": "Daminha",
+    "descricao": "Jogo de dama para ser jogado offline.",
+    "url": "https://github.com/JardelBrasiliano/daminha-game",
+    "topicos": ["React", "Context Api"]
   },
+  {
+    "name": "S-eta",
+    "descricao": "SETA (Simulador de Estação de tratamento de Água), game quiz de química.",
+    "url": "https://github.com/JardelBrasiliano/s-eta",
+    "topicos": ["React", "HTML", "CSS"]
+  },
+  {
+    "name": "Meu deck pokemon",
+    "descricao": "Consumindo a PokeAPI, crie seu deck e compartilhe com seus colegas.",
+    "url": "https://github.com/JardelBrasiliano/meu-deck-pokemon",
+    "topicos": ["React", "Firebase", "Redux", "Redux-saga", "Axios"]
+  }
 ]
 
 export default listaProjetos;
